2759b88bf4ee7dc03d07023d2e27fbb0cb4145b1
TAG-Epic <tagepicuwu@gmail.com>
2023-07-11 00:53:41 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,55 +1,50 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    crane.url = "github:ipetkov/crane";
-    crane.inputs.nixpkgs.follows = "nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, crane, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        craneLib = crane.lib.${system};
-      in
       with import nixpkgs { system = system; };
-        {
-            packages.default = stdenv.mkDerivation rec {
-            name = "btop";
-            version = "1.2.13";
-            src = self;
-            ADDFLAGS = with darwin.apple_sdk.frameworks;
-                lib.optional stdenv.isDarwin
-                "-F${IOKit}/Library/Frameworks/";
+      {
+        packages.default = stdenv.mkDerivation rec {
+          name = "btop";
+          version = "1.2.13";
+          src = self;
+          ADDFLAGS = with darwin.apple_sdk.frameworks;
+            lib.optional stdenv.isDarwin
+              "-F${IOKit}/Library/Frameworks/";
 
-            buildInputs = with pkgs; [fmt] ++ (with darwin.apple_sdk;
-                lib.optionals stdenv.isDarwin [
-                    frameworks.CoreFoundation
-                    frameworks.IOKit
-                ] ++ lib.optional (stdenv.isDarwin && stdenv.isx86_64) (
-                    # Found this explanation for needing to create a header directory for libproc.h alone.
-                    # https://github.com/NixOS/nixpkgs/blob/049e5e93af9bbbe06b4c40fd001a4e138ce1d677/pkgs/development/libraries/webkitgtk/default.nix#L154
-                    # TL;DR, the other headers in the include path for the macOS SDK is not compatible with the C++ stdlib and causes issues, so we copy
-                    # this to avoid those issues
-                    runCommand "${pname}_headers" { } ''
-                        install -Dm444 "${lib.getDev sdk}"/include/libproc.h "$out"/include/libproc.h
-                    ''
-                ));
+          buildInputs = with pkgs; [ fmt ] ++ (with darwin.apple_sdk;
+            lib.optionals stdenv.isDarwin [
+              frameworks.CoreFoundation
+              frameworks.IOKit
+            ] ++ lib.optional (stdenv.isDarwin && stdenv.isx86_64) (
+              # Found this explanation for needing to create a header directory for libproc.h alone.
+              # https://github.com/NixOS/nixpkgs/blob/049e5e93af9bbbe06b4c40fd001a4e138ce1d677/pkgs/development/libraries/webkitgtk/default.nix#L154
+              # TL;DR, the other headers in the include path for the macOS SDK is not compatible with the C++ stdlib and causes issues, so we copy
+              # this to avoid those issues
+              runCommand "${pname}_headers" { } ''
+                install -Dm444 "${lib.getDev sdk}"/include/libproc.h "$out"/include/libproc.h
+              ''
+            ));
 
-            installFlags = [ "PREFIX=$(out)" ];
+          installFlags = [ "PREFIX=$(out)" ];
 
-            postInstall = ''
-                ${removeReferencesTo}/bin/remove-references-to -t ${stdenv.cc.cc} $(readlink -f $out/bin/btop)
-            '';
+          postInstall = ''
+            ${removeReferencesTo}/bin/remove-references-to -t ${stdenv.cc.cc} $(readlink -f $out/bin/btop)
+          '';
 
-            meta = with lib; {
-                description = "A monitor of resources";
-                homepage = "https://github.com/aristocratos/btop";
-                changelog = "https://github.com/aristocratos/btop/blob/v${version}/CHANGELOG.md";
-                license = licenses.asl20;
-                platforms = platforms.linux ++ platforms.darwin;
-                maintainers = with maintainers; [ rmcgibbo ];
-            };
+          meta = with lib; {
+            description = "A monitor of resources";
+            homepage = "https://github.com/aristocratos/btop";
+            changelog = "https://github.com/aristocratos/btop/blob/v${version}/CHANGELOG.md";
+            license = licenses.asl20;
+            platforms = platforms.linux ++ platforms.darwin;
+            maintainers = with maintainers; [ rmcgibbo ];
+          };
 
         };
-    });
+      });
 }
